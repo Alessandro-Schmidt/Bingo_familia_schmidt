276ee7a4cdc7968e6289140f5b6dfaf0091057bd
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2022-12-29 22:40:30 -0300

    titles

--- bingo.py
+++ bingo.py
@@ -2,9 +2,17 @@ from random import shuffle
 from tabulate import tabulate
 import pyfiglet
 
-num_shuffle = []
+titulo_bingo = pyfiglet.figlet_format('Bingo\nFamília\nSchmidt', font="standard")
+click_to_beggin = pyfiglet.figlet_format('Clique para iniciar', font="bubble")
+print(titulo_bingo)
+
+input(click_to_beggin)
+
+numeros = []
 for i in range(1,100):
-    num_shuffle.append(i)
-    
-shuffle(num_shuffle)
-print(num_shuffle)
+    numeros.append(i)
+   
+shuffle(numeros) 
+#for a in numeros:
+
+print(numeros)
