81e56e7621cb4bf0c7197c39fc3d6e0aaab65df5
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2022-12-29 23:26:12 -0300

    Correção do bug

--- bingo.py
+++ bingo.py
@@ -16,6 +16,7 @@ shuffle(numeros)
 parada = ''
 i = 0
 mostrados = []
+print(numeros)
 
 while (i<len(numeros)) and parada!='bingo':
     mostrados.append(numeros[i])
@@ -25,7 +26,7 @@ while (i<len(numeros)) and parada!='bingo':
     print('Números já sorteados: '+ str(mostrados))
     i+=1
     parada = str(input('\033[0;30;42mAperte enter ou digite "bingo"\033[m\n'))
-    for i in range(1,4):
+    for a in range(1,4):
         print('.\n')
         sleep(0.8)
 
